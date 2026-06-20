d02859ed23b97068607ba9c1c2bb07e774e7ae9e
RRAHUL2006 <r3629296@gmail.com>
2026-06-20 22:44:02 +0530

    Fix DVC startup script

--- start.sh
+++ start.sh
@@ -4,17 +4,9 @@ set -e
 
 echo "Starting model download..."
 
-# Initialize DVC without Git
-dvc init --no-scm
-
-# Add Google Drive remote
-dvc remote add -f myremote gdrive://1T_8NdwTpFWIqz3aE_xMibUbwk1VgdexB
-
-# Configure service account
 dvc remote modify --local myremote gdrive_use_service_account true
 dvc remote modify --local myremote gdrive_service_account_json_file_path /etc/secrets/service-account.json
 
-# Download models
 dvc pull models/best_model.pkl.dvc
 dvc pull models/preprocessor.pkl.dvc
 
