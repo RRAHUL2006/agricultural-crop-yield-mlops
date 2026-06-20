284263cc429901b82278be9c4e67b69c1637872a
RRAHUL2006 <r3629296@gmail.com>
2026-06-20 22:32:33 +0530

    Fix DVC no-SCM startup for Render

--- start.sh
+++ start.sh
@@ -1,12 +1,20 @@
 #!/bin/sh
 
+set -e
+
 echo "Starting model download..."
 
-mkdir -p /app/models
+# Initialize DVC without Git
+dvc init --no-scm
 
+# Add Google Drive remote
+dvc remote add -f myremote gdrive://1T_8NdwTpFWIqz3aE_xMibUbwk1VgdexB
+
+# Configure service account
 dvc remote modify --local myremote gdrive_use_service_account true
 dvc remote modify --local myremote gdrive_service_account_json_file_path /etc/secrets/service-account.json
 
+# Download models
 dvc pull models/best_model.pkl.dvc
 dvc pull models/preprocessor.pkl.dvc
 
